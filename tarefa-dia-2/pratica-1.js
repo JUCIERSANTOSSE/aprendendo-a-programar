f61d8ccd35963ae642211326846f3e92c9990307
Jucier Santos <juciersantosadm@gmail.com>
2022-08-25 15:39:31 -0300

    Mais dados adicionados

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,8 +1,8 @@
 
-let seuNome = 'Jucier Santos'
+let seuNome = `Jucier Santos`
 let seuEmail = 'juciersantosadm@gmail.com'
 let suaProfissao = 'Futuro Programador'
 let suaIdade = 35
 let seuLugar = 'Canadá'
 
-console.log('Olá '+'seuNome '+ 'somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para '+'seuLugar '+'Confirma pra gente alguns dados? A sua idade é '+'suaIdade ', 'você é '+'suaProfissao'+'e o seu email é '+'seuEmail '+'Ficamos no aguardo. Muito obrigado e boa viagem!')
+    console.log(`Olá ` + seuNome + ` somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para `+ seuLugar +`. Confirma pra gente alguns dados? A sua idade é `+ suaIdade +` anos, você é `+ suaProfissao +` e o seu email é `+ seuEmail +`. Ficamos no aguardo. Muito obrigado e boa viagem!`)
