f0b4ae0cabdddb8d1bf2f8a94e2b14be248222b8
Jucier Santos <juciersantosadm@gmail.com>
2022-08-26 12:44:52 -0300

    Informações solicitadas

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -1,13 +1,22 @@
-
+//
 let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
 let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
 let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+//
+
+produtoA [`camiseta`] = 100
+produtoB [`perfume`] = 200
+produtoC [`sandália`] = 120
 
 
-let produtoA = {
-    nome: "camiseta", 
-    valor: 100.00, 
-    internacional: true
-}
-    console.log(produtoA.nome)
+    console.log(produtoA.nome + produtoA.valor * 1.2)
+    console.log(produtoB.nome + produtoB.valor * 1.2)
+    console.log(produtoC.nome + produtoC.valor * 1.12)
+
+    
+    
+
+    
+    
+
     
