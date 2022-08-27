26f62e5500ddebe35fc0104c17d7afc01f58db28
Jucier Santos <juciersantosadm@gmail.com>
2022-08-27 20:54:12 -0300

    Exercícios concluídos

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,14 @@
+let i = 1
+let multiplicador = 5
+let n = 10
+
+    while (i <= n) {
+       
+        let resultado = multiplicador * i;
+       
+        let textoDeExbicao = `${multiplicador} X ${i} = ${resultado}`;
+       
+        console.log(textoDeExbicao);
+       
+        i++ 
+    }
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,9 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+for (let i = 0; i < numeros.length; i++) {
+    
+    let numero = numeros[i];
+
+    if ( (numero % 2) != 0)
+    console.log(numero)
+}
